2df856e94466068baabfd23d7a1a37f5a91157f2
Vincent LAURENT <vince.laurent@gmail.com>
2018-01-27 16:18:02 +0100

    Gestion du formatage de l'email en javascript

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-     tel = $('.fa-phone').next().text().split(' ').reverse().join(' ');
+     var tel = $('.fa-phone').next().text().split(' ').reverse().join(' ');
      $('.fa-phone').next().text(tel);
      $('.fa-phone').next().attr('href', 'tel:+33' + tel.substring(1).replace(/ /g,''));
+
+     var email = $('.fa-envelope-o').next().text().replace('[at]', '@');
+     $('.fa-envelope-o').next().text(email);
+     $('.fa-envelope-o').next().attr('href', 'mailto:' + email);
 });
