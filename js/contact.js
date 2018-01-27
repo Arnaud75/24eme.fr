9534a049ebdf7844d21cb95d703209b3b84f17c1
Vincent LAURENT <vince.laurent@gmail.com>
2018-01-27 16:13:59 +0100

    tel: au lien de callto:

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
      tel = $('.fa-phone').next().text().split(' ').reverse().join(' ');
      $('.fa-phone').next().text(tel);
-     $('.fa-phone').next().attr('href', 'callto:+33' + tel.substring(1).replace(/ /g,''));
+     $('.fa-phone').next().attr('href', 'tel:+33' + tel.substring(1).replace(/ /g,''));
 });
-
